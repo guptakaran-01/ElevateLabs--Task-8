53c1edb96cbe27ea475e7c7b511de36a16d17006
guptakaran-01 <guptakaranmanoj@amityonline.com>
2026-03-02 18:56:36 +0530

    jenkins

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main {
     
     public static void main(String[] args) {
         System.out.println("Hi to jenkins file commited ");
-        
+        System.out.println("Something added ");
+
     }
 }
